89f6242d79e6c01cca7de92b1215cf3f1aebf587
Yasiru Randika <yrandika0103@gmail.com>
2026-06-03 12:18:27 +0100

    new bugs fixed

--- app/Jobs/ProcessStoryJob.php
+++ app/Jobs/ProcessStoryJob.php
@@ -45,11 +45,11 @@ class ProcessStoryJob implements ShouldQueue
 
 
         $polly = new PollyClient([
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => config('config.aws_region'),
             'version' => 'latest',
             'credentials' => [
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                'key' => config('config.aws.key'),
+                'secret' => config('config.aws.secret'),
             ]
         ]);
 

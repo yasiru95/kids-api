7783ed82b3802dbf57a555eac99826042983baaf
Yasiru Randika <yrandika0103@gmail.com>
2026-06-10 15:07:11 +0100

    add corns

---
+++ config/cors.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CORS Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://kidsstoryflix.co.uk',
+        'https://www.kidsstoryflix.co.uk',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
